62f37e325d4054b8f5f123596bff1e8248f3cdbc
Mulitha jayawardana <jmulitha@gmail.com>
2020-09-23 04:34:40 +0530

    Updated

---
+++ precache-manifest.09e0e7cfc3d483abc2c59626ab4f9d7d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd59507c0bda679468cbd04980eab538",
+    "url": "/Create-Your-Own-Covid-Hero-Image/index.html"
+  },
+  {
+    "revision": "683c4f9cf04373fb6204",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/css/main.4622634b.chunk.css"
+  },
+  {
+    "revision": "882cbee0d529a9bf7f45",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/js/2.728d3e12.chunk.js"
+  },
+  {
+    "revision": "716005237d5ce2003d1c8234ea0762ca",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/js/2.728d3e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683c4f9cf04373fb6204",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/js/main.454baa34.chunk.js"
+  },
+  {
+    "revision": "5c4925fe19252a0e6dc8",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/js/runtime-main.00484754.js"
+  },
+  {
+    "revision": "22f8567706b4c8fc6f4689d8d855bd65",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/Fakt-Bold.22f85677.woff"
+  },
+  {
+    "revision": "22f8567706b4c8fc6f4689d8d855bd65",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/Fakt-Bold.22f85677.woff2"
+  },
+  {
+    "revision": "79dd99c504334a418f36c4aa1bc6c1a8",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/Fakt-Normal.79dd99c5.woff"
+  },
+  {
+    "revision": "79dd99c504334a418f36c4aa1bc6c1a8",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/Fakt-Normal.79dd99c5.woff2"
+  },
+  {
+    "revision": "3f2b96bb3b0f15fbc951cf966e70ebb4",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/add-blue.3f2b96bb.svg"
+  },
+  {
+    "revision": "eb1dfe21977f488839ab14acd5a2f2d9",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/add.eb1dfe21.svg"
+  },
+  {
+    "revision": "3de717269204cd3beaf7e217d963bd87",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/arrow-blue2.3de71726.svg"
+  },
+  {
+    "revision": "29d567801660c94b824fcff6be1d0fb4",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/arrow2.29d56780.svg"
+  },
+  {
+    "revision": "6a87649fee8ba738164ac8e8599a0c01",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/close.6a87649f.svg"
+  },
+  {
+    "revision": "efd72981e0d4f34ca4d5b01fe48dfd7f",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/confetti-bg.efd72981.png"
+  },
+  {
+    "revision": "2ca15852c1eb4589d48b2a86d492491a",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/leituranews-roman_1-webfont.2ca15852.woff"
+  },
+  {
+    "revision": "2ca15852c1eb4589d48b2a86d492491a",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/leituranews-roman_1-webfont.2ca15852.woff2"
+  },
+  {
+    "revision": "b59a3030878147aac626c721f604d1c1",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/next.b59a3030.svg"
+  },
+  {
+    "revision": "fbca61ac62b8b233fef47608b5712331",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/overlay.fbca61ac.png"
+  },
+  {
+    "revision": "bfad45f760f9a4bdd442054bc06a6c67",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/previous.bfad45f7.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Create-Your-Own-Covid-Hero-Image/static/media/slick.f97e3bbf.svg"
+  }
+]);
